c141e3f531224739e5aff3dd24ee926a8b7571eb
Richard Guerrero <richjguerrero@gmail.com>
2026-04-30 15:14:00 -0700

    Initial Commit

---
+++ MicroBlueprint/MicroBlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroBlueprintApp.swift
+//  MicroBlueprint
+//
+//  Created by Richard Guerrero on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct MicroBlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
